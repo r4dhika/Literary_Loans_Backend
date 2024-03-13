eebeace11368109c9f0ab9f29ca5a67e9c2fcba8
RishmithaRachakonda <rachakondarishmitha@gmail.com>
2024-03-13 18:34:55 +0530

    list views are added

---
+++ literaryLoans/literaryLoans_app/views/Borrowedbooks.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+from ..models import BorrowRequest
+from ..serializers import BorrowRequestSerializer
+
+class BorrowedbooksListAPIView(generics.ListAPIView):
+    serializer_class=BorrowRequestSerializer
+
+    def get_queryset(self):
+        user_id=self.kwargs.get('user_id')
+        return BorrowRequest.objects.filter(borrower_id=user_id).filter(status='2').order_by('return_date')
--- literaryLoans/literaryLoans_app/views/book.py
+++ literaryLoans/literaryLoans_app/views/book.py
@@ -5,3 +5,9 @@ from ..serializers import BookSerializer
 class BookListAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class=BookSerializer
+
+
+class BookDestroyAPIView(generics.DestroyAPIView):
+    queryset=Book.objects.all()
+    serializer_class=BookSerializer
+    lookup_field='book_id'
