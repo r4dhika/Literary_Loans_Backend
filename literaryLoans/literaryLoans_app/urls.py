c19a535c92b5119e7e56698c29292464f69f71bc
r4dhika <radhika.maheshwari@desisascendeducare.in>
2024-03-15 09:09:51 +0530

    resolved merge conflicts

--- literaryLoans/literaryLoans_app/urls.py
+++ literaryLoans/literaryLoans_app/urls.py
@@ -15,6 +15,10 @@ urlpatterns = [
     path('destinations/<int:user_id>/', views.UserDestinations.as_view(), name='User Destinations'),
     path('calculateDistance/', views.CalculateDistance.as_view(), name = 'Estimated Distances'),
     path('auth/google/token/', views.google_token, name="login-with-google"),
-    path('borrowRequests/<int:user_id>/', views.BorrowRequestListAPIView.as_view(), name='Borrow Request'),
     path('auth/data/', views.user_data, name="user data"),
+    path('borrowRequests/<int:user_id>/', views.BorrowRequestListAPIView.as_view(), name='Borrow Request'),
+    path('books/delete/<int:pk>/', views.BookDestroyAPIView.as_view(), name = 'book Delete'),
+    path('borrowedbooks/<int:user_id>/', views.BorrowedbooksListAPIView.as_view(), name = 'Borrowed Books'),
+    path('lendedbooks/<int:user_id>/', views.LendedbooksListAPIView.as_view(), name = 'Lended Books'),
+    path('requestedbooks/<int:user_id>/', views.RequestedbooksListAPIView.as_view(), name = 'Requested Books')
 ]
--- literaryLoans/literaryLoans_app/views/__init__.py
+++ literaryLoans/literaryLoans_app/views/__init__.py
@@ -12,3 +12,7 @@ from .userAddresses import user_addresses
 from .userAddresses import UserDestinations
 from .auth import google_token
 from .auth import user_data
+from .book import BookDestroyAPIView
+from .Borrowedbooks import BorrowedbooksListAPIView
+from .Lendedbooks import LendedbooksListAPIView
+from .Requestedbooks import RequestedbooksListAPIView
